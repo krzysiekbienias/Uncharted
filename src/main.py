f34402060e12c9a3c3bb17d6027b602cd7fb2cd5
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2023-04-01 15:18:50 +0200

    Update main.py file.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,59 @@
 from uncharted.domain.yahooDataExtractor import YahooDataExtractor
+from uncharted.toolkit.IOToolKit import XlWingsTools
+from uncharted.domain.basicFinancialAnalysis import FinancialInstrument
 
-yahoo_data=YahooDataExtractor(tickers=['AAPL',
-                                        "BA",
-                                        "KO",
-                                        "IBM",
-                                        "DIS",
-                                        "MSFT"],
+import os
+
+# ------------------
+# Region: User customization
+# ------------------ 
+working_directory="/Users/krzysiekbienias/Documents/GitHub"
+output_directory="Uncharted/io"
+report_file_name="basic_analysis.xlsx"
+
+# -----------------------
+# Region: Input Output location
+# -----------------------
+io_path=os.path.join(working_directory,output_directory)
+if not os.path.isdir(io_path):
+        raise ValueError("Path you chosen does not exists!")
+# -----------------------
+# Region: Input Output location
+# -----------------------
+
+instruments=['AAPL',
+            "BA",
+            "KO",
+            "IBM",
+            "DIS",
+            "MSFT"]
+
+# ---------------------
+# Region: User customization
+# ---------------------
+
+report_book=XlWingsTools.createNewExcelFile(save_path=os.path.join(io_path,report_file_name),
+                                sheet_names=instruments)
+XlWingsTools.clearAllSpreadSheet(os.path.join(io_path,report_file_name))
+
+        
+
+
+print("File for presenting results has been prepared.")
+
+
+yahoo_data=YahooDataExtractor(tickers=instruments,
                                         start_period="2010-01-01",
                                         end_period="2022-10-30")
-yahoo_data.dfInfo(df=yahoo_data.close_prices_df)
+
+
+basic_financial_analysis=FinancialInstrument(yahoo_data_handler=yahoo_data,
+                                             xw_book=report_book
+                                             )
+
+print("THE END")
+
+
+
 
 
